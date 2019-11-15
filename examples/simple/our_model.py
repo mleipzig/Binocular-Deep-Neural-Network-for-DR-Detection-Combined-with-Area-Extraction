2b71d7af72a5d54b280c1967e218419f229f7910
xiangyu-liu <liuxiangyu999@sjtu.edu.cn>
2019-11-15 17:07:28 +0800

    modify the path

--- examples/simple/our_model.py
+++ examples/simple/our_model.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pandas as pd
 from PIL import Image
@@ -7,10 +8,11 @@ import efficientnet_pytorch
 from efficientnet_pytorch import EfficientNet
 import numpy as np
 
-PATH = "newNAS/Workspaces/DRLGroup/xiangyuliu/images"
-EXCEL = "newNAS/Workspaces/DRLGroup/xiangyuliu/label.xlsx"
+PATH = "/newNAS/Workspaces/DRLGroup/xiangyuliu/images"
+EXCEL = "/newNAS/Workspaces/DRLGroup/xiangyuliu/label.xlsx"
 
-
+# PATH = "/home/xiangyuliu/mnt/newNAS/Workspaces/DRLGroup/xiangyuliu/images"
+# EXCEL = "/home/xiangyuliu/mnt/newNAS/Workspaces/DRLGroup/xiangyuliu/label.xlsx"
 class Classifier():
     def __init__(self, image_base_path, label_path, lr=0.001, from_scrtch=True):
         super(Classifier, self).__init__()
@@ -19,7 +21,7 @@ class Classifier():
         self.image_base_path = image_base_path
         self.label_path = label_path
         self.model = EfficientNet.from_pretrained('efficientnet-b7', num_classes=5)
-        self.image_label_dict = self.load_label_dict()
+        self.image_label_dict = json.load(open("data_dict.json", "r"))
         self.tfms = transforms.Compose([transforms.Resize(size=(224, 224)), transforms.ToTensor(),
                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), ])
         self.total_sample_num = len(self.image_label_dict.values())
@@ -40,7 +42,6 @@ class Classifier():
             image = os.path.split(image)[1]
             if os.path.exists(os.path.join(PATH, image)):
                 image = self.tfms(Image.open(os.path.join(PATH, image))).unsqueeze(0)
-                print(image.shape)
                 image_array.append(image)
                 label_array.append(self.image_label_dict[image_path])
             if len(image_array) == batch_size:
@@ -65,25 +66,32 @@ class Classifier():
         accuracy = 0
         with torch.no_grad():
             for i in range(batch_size):
-                outputs = self.model(batch[i])
-                # print(outputs)
-                # Print predictions
+                outputs = self.model(batch[i].view((1,)+ batch[i].shape))
                 print('-----')
                 for idx in torch.topk(outputs, k=1).indices.squeeze(0).tolist():
                     prob = torch.softmax(outputs, dim=1)[0, idx].item()
-                    print(idx, labels[i], prob)
+                    print(idx, labels[i].item(), prob)
                     if idx == labels[i]:
                         accuracy += 1
         return accuracy / batch_size
 
+class Preprocess():
+    def __init__(self):
+        self.label_dict = json.load(open("data_dict.json", "r"))
+    def calculate_num_per_kind(self):
+        return json.load(open("num_per_kind.json", "r"))
+
 
 if __name__ == '__main__':
+    preprocess = Preprocess()
+    preprocess.calculate_num_per_kind()
     classifier = Classifier(PATH, EXCEL)
-    epoch = 10
-    batch_size = 20
+    epoch = 20
+    batch_size = 10
     for i in range(epoch):
         batch, labels = classifier.sample_minibatch(batch_size)
         loss = classifier.train(batch, labels)
         print(loss)
-        if i == 9:
-            print(classifier.evaluate(batch, labels))
+        if i == epoch-1:
+            test_batch, test_label = classifier.sample_minibatch(100)
+            print(classifier.evaluate(test_batch, test_label))
