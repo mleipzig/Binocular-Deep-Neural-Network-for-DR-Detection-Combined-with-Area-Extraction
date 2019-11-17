76f4031da43ec81a1a787152ddf57ecbd1e9b3ac
xiangyu-liu <liuxiangyu999@sjtu.edu.cn>
2019-11-17 18:09:50 +0800

    fix bugs

--- examples/simple/image_utils.py
+++ examples/simple/image_utils.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 from PIL import Image
 import json
-
+import time
 PATH = "/home/xiangyuliu/Downloads/images"
 EXCEL = "/home/xiangyuliu/Downloads/label.xlsx"
 
@@ -14,10 +14,11 @@ class DataLoader():
     def __init__(self, image_size, image_base_path, label_path):
         self.image_base_path = image_base_path
         self.label_path = label_path
-        self.image_label_dict = json.load(open("data_dict.json", "r"))
+        self.image_label_dict = json.load(open("local_data_dict.json", "r"))
         self.total_sample_num = len(self.image_label_dict.values())
         self.tfms = transforms.Compose([transforms.Resize(size=(image_size, image_size)),
-                                        transforms.ToTensor(), ])
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
 
     def sample_minibatch(self, batch_size, transform=True):
         image_array = []
@@ -31,7 +32,9 @@ class DataLoader():
                 if transform:
                     image = self.tfms(Image.open(os.path.join(self.image_base_path, image))).unsqueeze(0)
                 else:
-                    image = transforms.ToTensor()(image)
+                    image = transforms.Compose([transforms.Resize(size=(300, 300)),
+                                        transforms.ToTensor(), ])(Image.open(os.path.join(self.image_base_path, image)))
+                    image = image.unsqueeze(0)
                 image_array.append(image)
                 label_array.append(self.image_label_dict[image_path])
                 binary_label.append(0 if self.image_label_dict[image_path] < 0.5 else 1)
@@ -43,16 +46,14 @@ class DataLoader():
         return minibatch, torch.cat((label_tensor.view(1, -1), binary_label_tensor.view(1, -1)), dim=0)
 
     def test_transform(self):
-        image_batch, labels= self.sample_minibatch(50)
+        image_batch, labels= self.sample_minibatch(50, transform=True)
         for test_image, label in zip(image_batch, labels[0]):
-            print(label)
-            transform_image = self.tfms(test_image)
-
             true_image = transforms.ToPILImage()(test_image[0])
-            true_image.show()
+            if label>0.5:
+                time.sleep(5)
+                print(label)
+                true_image.show()
 
-            transform_image = transforms.ToPILImage()(transform_image[0])
-            transform_image.show()
 
     def load_label_dict(self):
         worksheet = pd.read_excel(self.label_path, sheet_name="Sheet2")
@@ -60,5 +61,5 @@ class DataLoader():
 
 
 if __name__ == '__main__':
-    dataset = DataLoader(PATH, EXCEL, 224)
+    dataset = DataLoader(300, PATH, EXCEL)
     dataset.test_transform()
