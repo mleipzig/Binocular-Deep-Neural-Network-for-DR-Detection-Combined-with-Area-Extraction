1419c104f814f67d9c1bf8c014552ed30440ffbd
xiangyu_liu <liuxiangyu999@sjtu.edu.cn>
2019-11-22 16:48:10 +0800

    add dataloader

--- examples/simple/main.py
+++ examples/simple/main.py
@@ -3,7 +3,8 @@ import torch
 from examples.simple.our_model import Classifier
 from examples.simple.trainer import Trainer
 from tensorboardX import SummaryWriter
-from .image_utils import DataLoader
+from .dataset import CustomDataset
+import numpy as np
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 PATH = "/newNAS/Workspaces/DRLGroup/xiangyuliu/images"
@@ -19,27 +20,36 @@ def adjust_learning_rate(optimizer, epoch, args):
         param_group['lr'] = lr
 
 
+def modify_labels(labels):
+    old_label_np = labels.numpy()
+    new_label_np = np.zeros(shape=old_label_np.shape)
+    for i in range(old_label_np.shape[0]):
+        new_label_np[i] = 0 if old_label_np[i] < 0.5 else 1
+    labels = np.concatenate([old_label_np.reshape(1, -1), new_label_np.reshape(1, -1)], axis=0)
+    return torch.from_numpy(labels)
+
+
 def main(args):
+    path_list = []
     classifier = Classifier(args).to(device)
     trainer = Trainer(classifier, args)
-    dataset = DataLoader(PATH, EXCEL)
+    train_data = CustomDataset(path_list)
+    train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=args.batch_size, shuffle=False)
     logger = SummaryWriter('log:' + str(args.batch_size) + "--" + str(args.image_size))
-    j = 0
+    iter = 0
     for i in range(args.epoch):
-        adjust_learning_rate(trainer.optimizer, i, args)
-        batch, labels = dataset.sample_minibatch(args.batch_size)
-        loss = trainer.train_a_batch_binary(batch, labels)
-        print(loss)
-        logger.add_scalar("loss", loss, i)
-        if i % args.eval_freq == args.eval_freq - 1:
-            test_batch, test_label = dataset.sample_minibatch(100)
-            print("----test accuracy")
-            test_accuracy = trainer.evaluate_binary(test_batch, test_label)
-            print("----train accuracy")
-            train_accuracy = trainer.evaluate_binary(batch, labels)
-            print("test_accuracy:", test_accuracy, " train_accuracy:", train_accuracy)
-            logger.add_scalar("accuracy", test_accuracy, j)
-            j += 1
+        for image_batch, labels in train_loader:
+            adjust_learning_rate(trainer.optimizer, iter, args)
+            labels = modify_labels(labels)
+            # we need the label of shape (?, 2)
+            loss = trainer.train_a_batch_binary(image_batch, labels)
+            print(loss)
+            logger.add_scalar("loss", loss, i)
+            if iter % args.eval_freq == args.eval_freq - 1:
+                print("----test train results")
+                train_accuracy = trainer.evaluate_binary(image_batch, labels)
+                print("train_accuracy:", train_accuracy)
+            iter += 1
 
 
 # Todo: (1)data preprocess(add more samples and normalize) (2)partition the data set (3)multiprocess (4) try gpu version (5) visualize the figure
@@ -47,7 +57,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
     parser.add_argument("--lr", default=0.1, type=float)
     parser.add_argument("--batch_size", default=48, type=int)
-    parser.add_argument("--epoch", default=10000, type=int)
+    parser.add_argument("--epoch", default=3, type=int)
     parser.add_argument("--eval_freq", default=50, type=int)
     parser.add_argument("--from_scratch", default=True, action="store_false")
     parser.add_argument("--image_size", default=300, type=int)
