6c46cf5d2e3c6f13ce05f3b06e43841db8188810
xiangyu_liu <liuxiangyu999@sjtu.edu.cn>
2019-12-03 23:15:21 +0800

    fix a minor

--- examples/simple/test_model.py
+++ examples/simple/test_model.py
@@ -6,12 +6,11 @@ from examples.simple.main import modify_labels
 from examples.simple.our_model import Classifier
 from examples.simple.trainer import Trainer, device
 from examples.simple.dataset import CustomDataset
-import numpy as np
 
 path_list = None
+save_path = "/newNAS/Workspaces/DRLGroup/xiangyuliu/EfficientNet-PyTorch/examples/simple/logs/four/3/load locallyFalse-squeezeFalse/300-48-1e-10/run1/param_1700.pt"
 
 def main(args):
-    save_path = "/newNAS/Workspaces/DRLGroup/xiangyuliu/EfficientNet-PyTorch/examples/simple/logs/four/3/load locallyFalse-squeezeFalse/300-48-1e-10/run1/param_1700.pt"
     model_dict = {"resnet18": torchvision.models.resnet18(pretrained=args.pretrain),
                   "resnet34": torchvision.models.resnet34(pretrained=args.pretrain),
                   "resnet50": torchvision.models.resnet50(pretrained=args.pretrain),
@@ -25,24 +24,27 @@ def main(args):
                   "wide_resnet101_2": torchvision.models.wide_resnet101_2(pretrained=args.pretrain)}
 
     if args.model_type == "binary":
+        args.image_size = 224
         classifier = Classifier(args).to(device)
     elif args.model_type == "four":
         classifier = model_dict[args.model_detail].to(device)
+    else:
+        print("no such model")
     classifier.load_state_dict(torch.load(save_path))
     trainer = Trainer(classifier, args)
 
     test_data = CustomDataset(path_list, img_size=args.image_size, model_type=args.model_type)
     test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=args.batch_size, shuffle=True,
                                                num_workers=2)
-    test_result = []
+    true_len = 0
     total_len = 0
     for image_batch, labels in test_loader:
         labels = modify_labels(labels)
         test_accuracy, _ = trainer.evaluate(image_batch, labels)
         print("test accuracy:", test_accuracy)
-        test_result.append(test_accuracy*image_batch.shape[0])
+        true_len += test_accuracy*image_batch.shape[0]
         total_len += image_batch.shape[0]
-    print("final accuracy", np.sum(np.array(test_result))/total_len)
+    print("final accuracy", true_len/total_len)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
@@ -54,6 +56,7 @@ if __name__ == '__main__':
     parser.add_argument("--save_freq", default=100, type=int)
     parser.add_argument("--squeeze", default=False, action="store_true")
     parser.add_argument("--model_scale", default=0, type=int)
+
     parser.add_argument("--image_size", default=300, type=int)
     parser.add_argument("--model_type", default="four", type=str)
     parser.add_argument("--model_detail", default="resnet18", type=str)
